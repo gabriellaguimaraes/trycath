9d90ac25a6c5770ab4f3ea5962dd787284bb324c
Gabriella <gabriellalobato34@gmail.com>
2023-06-06 14:33:12 -0400

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(  "Informe o primeiro valor! ");
+int a = Convert.ToInt32( Console.ReadLine() );
+
+try
+{
+    Console.WriteLine("Informe o segundo valor!");
+    int b= Convert.ToInt32( Console.ReadLine() );   
+    int r = a / b;
+    Console.WriteLine($"O resultado é {r}");
+}catch( DivideByZeroException   ex)
+{
+    Console.WriteLine($"Não é permitido divisão por zero! {ex.Message} ");
+
+    
+}catch(InvalidCastException ex)
+{
+    Console.WriteLine(  $"Conversão de string para letra não é permitido! {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine( $"Ocorreu um erro {ex.Message}");
+}
+finally
+{
+    Console.WriteLine(  "Aqui passa de qualquer forma!");
+}
